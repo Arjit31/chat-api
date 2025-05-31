65449f9bc27a61ab696ae311da35e3f96057ed2a
Arjit31 <apk20023110@gmail.com>
2025-05-31 14:36:46 +0530

    make zod validation stricter

--- src/types/User.ts
+++ src/types/User.ts
@@ -2,15 +2,15 @@ import {z} from "zod"
 
 export const UserSignupSchema = z.object({
     name: z.string().min(2),
-    username: z.string().min(2),
-    password: z.string().min(5)
+    username: z.string().min(3).refine(s => !s.includes(' '), 'No Spaces!'),
+    password: z.string().min(5).refine(s => !s.includes(' '), 'No Spaces!')
 })
 
 export type UserSignupType = z.infer<typeof UserSignupSchema>
 
 export const UserSigninSchema = z.object({
-    username: z.string().min(2),
-    password: z.string().min(5)
+    username: z.string().min(3).refine(s => !s.includes(' '), 'No Spaces!'),
+    password: z.string().min(5).refine(s => !s.includes(' '), 'No Spaces!')
 })
 
 export type UserSigninType = z.infer<typeof UserSigninSchema>
