78ce995adb80c4fb611494e1ca6d3c4c01206ed1
Arjit31 <apk20023110@gmail.com>
2025-06-01 11:28:46 +0530

    add unicast router

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { authRouter } from "./authRoute";
 import { broadcastRouter } from "./broadcastRoute";
+import { unicastRouter } from "./unicastRoute";
 
 const express = require("express");
 
@@ -8,6 +9,7 @@ export const rootRouter = express.Router();
 
 rootRouter.use("/auth", authRouter)
 rootRouter.use("/broadcast", broadcastRouter)
+rootRouter.use("/unicast", unicastRouter)
 
 rootRouter.get("/", function (req : Request, res: Response) {
   res.send("Root home page");
