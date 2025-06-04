b971c51a00822f8c3ecde71691f92522f0c44a7a
Arjit31 <apk20023110@gmail.com>
2025-06-04 12:39:47 +0530

    add heartbeat mechanism

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,58 @@ app.use("/api/v1", rootRouter);
 const mapId = new Map<string, WebSocket>();
 const objId = { map: mapId };
 
-wss.on("connection", function connection(socket) {
-  // authenticate user on connection
+interface ExtendedWebSocket extends WebSocket {
+  isAlive?: boolean;
+  heartbeatTimer?: NodeJS.Timeout;
+}
+
+wss.on("connection", function connection(socket: ExtendedWebSocket) {
+  console.log("New WebSocket connection established");
+  
+  socket.isAlive = true;
+  
+
+  const startHeartbeat = () => {
+    socket.heartbeatTimer = setInterval(() => {
+      if (socket.isAlive === false) {
+        console.log("Client didn't respond to ping, terminating connection");
+        clearInterval(socket.heartbeatTimer);
+        return socket.terminate();
+      }
+      
+      socket.isAlive = false;
+      socket.ping();
+      console.log("Ping sent to client");
+    }, 30000);
+  };
+
+  socket.on("pong", function() {
+    console.log("Pong received from client");
+    socket.isAlive = true;
+  });
+
+  socket.on("ping", function() {
+    console.log("Ping received from client");
+    socket.pong();
+  });
+
   socket.on("error", console.error);
+  
   socket.on("message", async function message(data) {
+    socket.isAlive = true;
     await rootHandler(wss, socket, objId, data);
   });
 
-  socket.send(`{"text": "something"}`);
+  socket.on("close", function() {
+    console.log("WebSocket connection closed");
+    if (socket.heartbeatTimer) {
+      clearInterval(socket.heartbeatTimer);
+    }
+  });
+
+  startHeartbeat();
+  
+  socket.send(`{"text": "connection established"}`);
 });
 
 server.listen(3000, () => {
